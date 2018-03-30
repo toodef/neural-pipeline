dd19c78030ce4f0a619d91bf086d1529ac447691
toodef <fedotov.anton.af@gmail.com>
2018-03-30 23:19:19 +0300

    image conveyor tests passed

--- image_conveyor.py
+++ image_conveyor.py
@@ -6,7 +6,7 @@ from abc import ABCMeta, abstractmethod
 
 class ImageLoader(metaclass=ABCMeta):
     @abstractmethod
-    def load(self, path: str):
+    def load(self, path: {}):
         """
         Load image
         :param path: path to image
@@ -34,8 +34,8 @@ class ImageConveyor:
         self.__images_bucket_size = images_bucket_size
         self.__image_loader = image_loader
         self.__image_pathes = pathes
-        self.__images_buffer = None
         self.__cur_index = 0
+        self.__images_buffers = [None, self.__load_buffer()]
 
     def load(self, path: str):
         """
@@ -46,8 +46,10 @@ class ImageConveyor:
         self.__image_loader.load(path)
 
     def __getitem__(self, index):
+        if (index - 1) * self.__images_bucket_size >= len(self.__image_pathes):
+            raise IndexError
         self.__swap_buffers()
-        return self.__images_buffer
+        return self.__images_buffers[0]
 
     def __enter__(self):
         return self
@@ -55,11 +57,15 @@ class ImageConveyor:
     def __exit__(self, exc_type, exc_val, exc_tb):
         pass
 
-    def __swap_buffers(self):
+    def __load_buffer(self):
         new_buffer = []
         for i in range(self.__images_bucket_size):
-            if self.__cur_index + i == len(self.__image_pathes):
+            if self.__cur_index == len(self.__image_pathes):
                 break
-            new_buffer.append(self.__image_loader.load(self.__image_pathes[self.__cur_index + i]))
-        self.__cur_index += self.__images_bucket_size
-        self.__images_buffer = new_buffer
+            new_buffer.append(self.__image_loader.load(self.__image_pathes[self.__cur_index]))
+            self.__cur_index += 1
+        return new_buffer
+
+    def __swap_buffers(self):
+        new_buffer = self.__load_buffer()
+        self.__images_buffers = [self.__images_buffers[1], new_buffer]
---
+++ tests.py
@@ -0,0 +1,28 @@
+import unittest
+
+from image_conveyor import ImageConveyor, ImageLoader
+
+
+class ImageConveyorTest(unittest.TestCase):
+    class TestImageLoader(ImageLoader):
+        def load(self, path: {}):
+            path['additional_data'] = path['path'] + "add"
+            return path
+
+    def test_get_data_test(self):
+        PATHES_NUM = 23
+        BUCKET_SIZE = 4
+        pathes = [{'path': str(path), 'some_data': path * 31} for path in range(PATHES_NUM)]
+        getting_pathes = []
+        with ImageConveyor(self.TestImageLoader(), pathes, BUCKET_SIZE) as conveyor:
+            for images in conveyor:
+                for img in images:
+                    getting_pathes.append(img)
+
+        print(pathes)
+        print(getting_pathes)
+        self.assertEqual(len(pathes), len(getting_pathes))
+
+
+if __name__ == '__main__':
+    unittest.main()
