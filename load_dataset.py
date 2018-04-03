bdb21f438b1507703e2e661f58e709347a392065
Anton Fedotov <a.fedotov@geoscan.lan>
2018-04-03 18:54:22 +0300

    rename main to load dataset.py cause it is it

--- load_dataset.py
+++ load_dataset.py
@@ -3,25 +3,23 @@ import sys
 import time
 import cv2
 
-from image_conveyor import ImageConveyor, ImageLoader, UrlLoader
+from image_conveyor import ImageConveyor, UrlLoader
 import json
 
 
-class TestLoader(ImageLoader):
-    def load(self, url):
-        return url
-
-
-def main():
-    with open("validation.json", 'r') as file:
+def load(data_file_path: str):
+    with open(data_file_path + ".json", 'r') as file:
         data = json.load(file)
 
     pathes = []
-    for i, item in enumerate(data['images']):
-        pathes.append({'path': item['url'][0], 'label_id': data['annotations'][item['image_id'] - 1]['label_id'],
-                       'image_id': item['image_id']})
+    label_ids = {}
+    for a in data['annotations']:
+        label_ids[a['image_id']] = a['label_id']
 
-    with ImageConveyor(UrlLoader(), pathes, 1000) as conveyor, open('log.txt', 'w') as log_file:
+    for img in data['images']:
+        pathes.append({'path': img['url'][0], 'label_id': label_ids[img['image_id']], 'image_id': img['image_id']})
+
+    with ImageConveyor(UrlLoader(), pathes, 1000) as conveyor, open(data_file_path + '_log.log', 'w') as log_file:
         i = 0
         start_time = time.time()
         loaded_num, failed_num = 0, 0
@@ -37,7 +35,7 @@ def main():
                     print(string, file=sys.stderr)
                     log_file.write(string + "\n")
                     continue
-                dir = "validation\\{}".format(img['label_id'])
+                dir = data_file_path + "\\{}".format(img['label_id'])
                 if not os.path.exists(dir):
                     os.makedirs(dir)
                 try:
@@ -65,11 +63,12 @@ def main():
                                                                                 failed_num,
                                                                                 100 * failed_num / len(pathes)))
 
-        with open("failed_urls_log.txt", 'w') as url_log_file:
+        with open(data_file_path + "_failed_urls_log.log", 'w') as url_log_file:
             for url in failed_url:
-                url_log_file.write(url)
+                url_log_file.write(url + "\n")
             url_log_file.flush()
 
 
 if __name__ == "__main__":
-    main()
+    load('validation')
+
