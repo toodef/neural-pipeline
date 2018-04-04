55d2846feddbda7247e7251a853b522945dd0685
Anton Fedotov <a.fedotov@geoscan.lan>
2018-04-04 18:03:39 +0300

    add train.py

---
+++ train.py
@@ -0,0 +1,56 @@
+from image_conveyor import ImageConveyor, PathLoader
+from image_processor import ImageProcessor
+import os
+import cv2
+import numpy as np
+
+train_dir = 'train'
+validation_dir = 'validation'
+image_size = 256
+
+classes = [int(dir) for dir in os.listdir(train_dir)]
+
+
+def get_pathes(directory):
+    res = []
+    for cur_class in classes:
+        res += [{'path': os.path.join(os.path.join(directory, str(cur_class)), file), 'label_id': cur_class}
+                for file in
+                os.listdir(os.path.join(directory, str(cur_class)))]
+    return res
+
+
+train_pathes = get_pathes(train_dir)
+validation_pathes = get_pathes(validation_dir)
+
+img_processor = ImageProcessor(len(classes), len(train_pathes), [image_size, image_size, 3])
+
+last_train_images = []
+
+
+def on_epoch():
+    accuracy = img_processor.get_accuracy(last_train_images)
+    with ImageConveyor(PathLoader(), validation_pathes, 32) as conveyor:
+        loss_values = []
+        valid_accuracies = []
+        for images in conveyor:
+            for img in images:
+                img['object'] = cv2.resize(img['object'], (image_size, image_size), 0, 0, cv2.INTER_LINEAR)
+            loss_values.append(img_processor.get_loss_value(images))
+            valid_accuracies.append(img_processor.get_accuracy(images))
+
+    epoch = img_processor.get_cur_epoch()
+    valid_acc = np.mean(np.array(valid_accuracies))
+    loss = np.mean(np.array(loss_values))
+    print("Epoch: {}, Training accuracy: {}, Validation accuracy {}, validation loss: {}".format(epoch, accuracy,
+                                                                                                 valid_acc, loss))
+
+
+img_processor.set_on_epoch(on_epoch)
+
+with ImageConveyor(PathLoader(), train_pathes, 32) as conveyor:
+    for images in conveyor:
+        for img in images:
+            img['object'] = cv2.resize(img['object'], (image_size, image_size), 0, 0, cv2.INTER_LINEAR)
+        last_train_images = images
+        img_processor.train_batch(images)
