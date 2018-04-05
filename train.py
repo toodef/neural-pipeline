43d3b9067df9ea65015e64918a753c8d36ea0fe3
Anton Fedotov <a.fedotov@geoscan.lan>
2018-04-05 17:04:19 +0300

    add correct epoch defenition option

--- train.py
+++ train.py
@@ -1,12 +1,17 @@
+import time
+
 from image_conveyor import ImageConveyor, PathLoader
 from image_processor import ImageProcessor
 import os
 import cv2
 import numpy as np
+from random import shuffle
 
 train_dir = 'train'
 validation_dir = 'validation'
-image_size = 256
+image_size = 128
+images_part = 32
+result_path = 'result\\furniture_segmentation'
 
 classes = [int(dir) for dir in os.listdir(train_dir)]
 
@@ -21,19 +26,26 @@ def get_pathes(directory):
 
 
 train_pathes = get_pathes(train_dir)
+shuffle(train_pathes)
 validation_pathes = get_pathes(validation_dir)
 
-img_processor = ImageProcessor(len(classes), len(train_pathes), [image_size, image_size, 3])
+img_processor = ImageProcessor(len(classes), len(train_pathes), [image_size, image_size, 3], epoch_every_train_num=1000)
 
 last_train_images = []
 
+start_time = None
+
 
 def on_epoch():
+    img_processor.save_state(result_path)
+
     accuracy = img_processor.get_accuracy(last_train_images)
-    with ImageConveyor(PathLoader(), validation_pathes, 32) as conveyor:
+    with ImageConveyor(PathLoader(), validation_pathes, images_part) as conveyor:
         loss_values = []
         valid_accuracies = []
         for images in conveyor:
+            if len(images) < images_part:
+                continue
             for img in images:
                 img['object'] = cv2.resize(img['object'], (image_size, image_size), 0, 0, cv2.INTER_LINEAR)
             loss_values.append(img_processor.get_loss_value(images))
@@ -42,14 +54,18 @@ def on_epoch():
     epoch = img_processor.get_cur_epoch()
     valid_acc = np.mean(np.array(valid_accuracies))
     loss = np.mean(np.array(loss_values))
-    print("Epoch: {}, Training accuracy: {}, Validation accuracy {}, validation loss: {}".format(epoch, accuracy,
-                                                                                                 valid_acc, loss))
+    print("Epoch: {}, Training accuracy: {:>6.1%}, Validation accuracy {:>6.1%}, validation loss: {:.3f},  Time: {:.2f} sec".format(epoch, accuracy, valid_acc, loss, time.time() - start_time))
 
 
 img_processor.set_on_epoch(on_epoch)
 
-with ImageConveyor(PathLoader(), train_pathes, 32) as conveyor:
+
+with ImageConveyor(PathLoader(), train_pathes, images_part) as conveyor:
+    conveyor.set_iterations_num(len(train_pathes) * 100)
+    start_time = time.time()
     for images in conveyor:
+        if len(images) < images_part:
+            continue
         for img in images:
             img['object'] = cv2.resize(img['object'], (image_size, image_size), 0, 0, cv2.INTER_LINEAR)
         last_train_images = images
