9779c78f4a5880f93e011ccb0cbd2514a1192671
Anton Fedotov <a.fedotov@geoscan.lan>
2018-04-25 11:10:37 +0300

    fix Monitor writing

--- data_processor/monitoring.py
+++ data_processor/monitoring.py
@@ -1,3 +1,5 @@
+import os
+
 from tensorboardX import SummaryWriter
 
 from data_processor import DataProcessor
@@ -6,6 +8,7 @@ from data_processor import DataProcessor
 class Monitor:
     def __init__(self, images_num: int):
         dir = "workdir\\logs"
+        os.makedirs(dir, exist_ok=True)
         self.__writer = SummaryWriter(dir)
         self.__images_num = images_num
 
@@ -13,8 +16,8 @@ class Monitor:
         self.__update_tensordoard(epoch, data_processor)
 
     def __update_tensordoard(self, epoch: int, data_processor: DataProcessor):
-        self.__writer.add_scalar('train\\loss', data_processor.get_loss_value(self.__images_num), global_step=epoch)
-        self.__writer.add_scalar('train\\accuracy', data_processor.get_accuracy(self.__images_num), global_step=epoch)
+        self.__writer.add_scalar('train/loss', data_processor.get_loss_value(self.__images_num), global_step=epoch)
+        self.__writer.add_scalar('train/accuracy', data_processor.get_accuracy(self.__images_num), global_step=epoch)
 
     def close(self):
         self.__writer.close()
