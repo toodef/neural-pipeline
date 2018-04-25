4aee6a3d60cd65e3721ef7d9ecd3f67538bf83c3
Anton Fedotov <a.fedotov@geoscan.lan>
2018-04-25 16:30:20 +0300

    another try

--- train.py
+++ train.py
@@ -17,27 +17,26 @@ def main():
     batch_size = int(config['data_conveyor']['batch_size'])
     threads_num = int(config['data_conveyor']['threads_num'])
 
-    data_size = config['data_conveyor']['data_size'][:2]
+    data_size = config['data_conveyor']['data_size']
 
     traindir = os.path.join(config['workdir_path'], 'train')
     valdir = os.path.join(config['workdir_path'], 'validation')
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 
-    train_folder = datasets.ImageFolder(traindir, transforms.Compose([
-        transforms.RandomResizedCrop(224),
-        # transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        normalize,
-    ]))
     train_loader = torch.utils.data.DataLoader(
-        train_folder,
+        datasets.ImageFolder(traindir, transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            # transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ])),
         batch_size=batch_size, shuffle=True,
         num_workers=threads_num, pin_memory=True)
 
     val_loader = torch.utils.data.DataLoader(
         datasets.ImageFolder(valdir, transforms.Compose([
-            transforms.Scale(256),
+            # transforms.Resize(data_size[:2]),
             # transforms.CenterCrop(224),
             transforms.ToTensor(),
             normalize,
