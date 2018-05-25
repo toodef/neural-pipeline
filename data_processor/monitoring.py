f2168f7b74398db9ec32c494311cd275a28eb427
toodef <fedotov.anton.af@gmail.com>
2018-05-25 11:29:47 +0300

    remove lr and data size from monitoring name

--- data_processor/monitoring.py
+++ data_processor/monitoring.py
@@ -6,7 +6,7 @@ from tensorboardX import SummaryWriter
 class Monitor:
     def __init__(self, config: {}):
         dir = os.path.join("workdir", "logs")
-        dir = os.path.join(dir, "{}_{}_{:2f}_{}".format(config['network']['architecture'], config['network']['optimizer'], config['network']['learning_rate'], config['data_conveyor']['data_size'][0]))
+        dir = os.path.join(dir, "{}_{}".format(config['network']['architecture'], config['network']['optimizer']))
         if os.path.exists(dir) and os.path.isdir(dir):
             idx = 0
             tmp_dir = dir + "_v{}".format(idx)
