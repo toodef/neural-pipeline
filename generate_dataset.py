19b9b83d43c9d50690e360b58c09960d2a724df3
toodef <fedotov.anton.af@gmail.com>
2018-07-03 22:43:49 +0300

    make pathes as related by workdir

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,14 +1,15 @@
 import json
 import os
 
-validation_dir = r"C:\workspace\nn_projects\furniture_segmentation\workdir\validation"
-train_dir = r"C:\workspace\nn_projects\furniture_segmentation\workdir\train"
+target_dir = r"Z:\datasets\fure"
+validation_dir = r"Z:\datasets\fure\validation"
+train_dir = r"Z:\datasets\fure\train"
 
 
 def get_pathes(directory, classes):
     res = []
     for cur_class in classes:
-        res += [{'path': os.path.join(os.path.join(directory, str(cur_class)), file), 'target': int(cur_class) - 1}
+        res += [{'path': os.path.relpath(os.path.join(os.path.join(directory, str(cur_class)), file), target_dir), 'target': int(cur_class) - 1}
                 for file in os.listdir(os.path.join(directory, str(cur_class)))]
     return res
 
@@ -18,10 +19,12 @@ train_dataset = {'labels': {str(l): l - 1 for l in train_classes}, 'data': get_p
 validation_classes = [int(d) for d in os.listdir(validation_dir)]
 validation_dataset = {'labels': {str(l): l - 1 for l in train_classes}, 'data': get_pathes(validation_dir, validation_classes)}
 
-with open(r'C:\workspace\nn_projects\furniture_segmentation\workdir\train.json', 'w') as out:
+overall_dataset = {'labels': dict(train_dataset['labels'], **validation_dataset['labels']), 'data': train_dataset['data'] + validation_dataset['data']}
+
+
+with open(os.path.join(target_dir, 'train.json'), 'w') as out:
     json.dump(train_dataset, out, indent=2)
-with open(r'C:\workspace\nn_projects\furniture_segmentation\workdir\validation.json', 'w') as out:
+with open(os.path.join(target_dir, 'validation.json'), 'w') as out:
     json.dump(validation_dataset, out, indent=2)
-with open(r'C:\workspace\nn_projects\furniture_segmentation\workdir\datast.json', 'w') as out:
-    json.dump(train_dataset, out, indent=2)
-    json.dump(validation_dataset, out, indent=2)
+with open(os.path.join(target_dir, 'dataset.json'), 'w') as out:
+    json.dump(overall_dataset, out, indent=2)
