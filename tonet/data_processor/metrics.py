6c2af605449b582bca8b69ba692d04b7d000c901
toodef <fedotov.anton.af@gmail.com>
2018-07-26 20:56:43 +0300

    add metrics.py

---
+++ tonet/data_processor/metrics.py
@@ -0,0 +1,40 @@
+import torch
+
+eps = 1e-3
+
+
+def dice_loss(preds, trues, weight=None, is_average=True):
+    preds = preds.contiguous()
+    trues = trues.contiguous()
+    num = preds.size(0)
+    preds = preds.view(num, -1)
+    trues = trues.view(num, -1)
+    if weight is not None:
+        w = torch.autograd.Variable(weight).view(num, -1)
+        preds = preds * w
+        trues = trues * w
+    intersection = (preds * trues).sum(1)
+    scores = (2. * intersection + eps) / (preds.sum(1) + trues.sum(1) + eps)
+
+    if is_average:
+        score = scores.sum() / num
+        return float(torch.clamp(score, 0., 1.))
+    else:
+        return scores
+
+
+def jaccard(preds, trues, weight=None, is_average=True):
+    num = preds.size(0)
+    preds = preds.view(num, -1)
+    trues = trues.view(num, -1)
+    if weight is not None:
+        w = torch.autograd.Variable(weight).view(num, -1)
+        preds = preds * w
+        trues = trues * w
+    intersection = (preds * trues).sum(1)
+    scores = (intersection + eps) / ((preds + trues).sum(1) - intersection + eps)
+
+    score = scores.sum()
+    if is_average:
+        score /= num
+    return float(torch.clamp(score, 0., 1.))
