f26134620c5ea337475551fdb0c3fe97784a3523
toodef <fedotov.anton.af@gmail.com>
2018-11-19 20:49:06 +0300

    new changes

--- neural_pipeline/data_processor/data_processor.py
+++ neural_pipeline/data_processor/data_processor.py
@@ -1,13 +1,19 @@
+import json
+import numpy as np
+
 import torch
 from tqdm import tqdm
 
 from neural_pipeline.data_processor.model import Model
-from neural_pipeline.train_pipeline.train_pipeline import AbstractTrainPipeline
+from neural_pipeline.data_processor.state_manager import StateManager
+from neural_pipeline.train_pipeline import TrainPipeline
+from neural_pipeline.utils.file_structure_manager import FileStructManager
+from neural_pipeline.utils.utils import dict_recursive_bypass
 
 
 class DataProcessor:
     """
-    Class, that get data from data_producer and process it:
+    Class, that get data from data_conveyor and process it:
     1) Train or predict data
     2) Provide monitoring (showing metrics to console and tesorboard)
     """
@@ -17,13 +23,17 @@ class DataProcessor:
         Learning rate manage strategy.
         This class provide lr decay by loss values. If loss doesn't update minimum throw defined number of steps - lr decay to defined coefficient
         """
-        def __init__(self, config: {}):
+
+        def __init__(self, config: {}, is_continue: bool = False):
             """
             :param config: learning rate config
             """
             self.__value = float(config['learning_rate']['start_value'])
             self.__decrease_coefficient = float(config['learning_rate']['decrease_coefficient'])
             self.__steps_before_decrease = config['learning_rate']['steps_before_decrease']
+            if is_continue and 'first_steps_before_decrease' in config['learning_rate']:
+                self.__decrease_after_first_steps_num = config['learning_rate']['first_steps_before_decrease']
+                self.__first_decrease_coefficient = config['learning_rate']['first_decrease_coefficient']
             self.__cur_step = 0
             self.__min_loss = None
             self.__just_decreased = False
@@ -34,10 +44,15 @@ class DataProcessor:
             :param cur_loss: current loss value
             :return: learning rate value
             """
+            self.__just_decreased = False
+
+            if hasattr(self, "_LearningRate__decrease_after_first_steps_num") and self.__cur_step == self.__decrease_after_first_steps_num:
+                self.set_value(self.__value / self.__first_decrease_coefficient)
+
             if cur_loss is None:
+                self.__cur_step += 1
                 return self.__value
 
-            self.__just_decreased = False
             if self.__min_loss is None:
                 self.__min_loss = cur_loss
 
@@ -48,87 +63,142 @@ class DataProcessor:
 
             if self.__cur_step > 0 and (self.__cur_step % self.__steps_before_decrease) == 0:
                 self.__value /= self.__decrease_coefficient
+                self.__min_loss = None
                 print('Decrease lr to', self.__value)
                 self.__just_decreased = True
+                self.__cur_step = 0
+                return self.__value
 
             self.__cur_step += 1
             return self.__value
 
+        def set_value(self, value):
+            self.__value = value
+            self.__cur_step = 0
+            self.__min_loss = None
+
         def lr_just_decreased(self) -> bool:
             return self.__just_decreased
 
-    def __init__(self, model: Model, train_pipeline: AbstractTrainPipeline, for_train: bool):
+    def __init__(self, model, train_pipeline: TrainPipeline, config: {}, file_struct_manager: FileStructManager, is_cuda=True, for_train: bool = True):
         """
         :param model: model object
-        :param train_pipeline: train pipeline object
-        :param for_train: is data processor created for train model
+        :param config: data processor conifig
+        :param file_struct_manager: file stucture manager
         """
-        self.__model = model
-        self.__train_pipeline = train_pipeline
-        self.__is_train = for_train
+
+        self.__is_cuda = is_cuda
+        self.__file_struct_manager = file_struct_manager
+
+        self.__model = Model(model, file_struct_manager)
 
         if for_train:
-            self.__model.model().train()
-        else:
-            self.__model.model().eval()
+            self.metrics_processor = train_pipeline.get_metrics_procesor()
+            self.__criterion = train_pipeline.get_loss()
 
-        self.__optimizer = None
-        self.__loss = None
+            if self.__is_cuda:
+                self.__criterion.to('cuda:0')
 
-    def set_optimizer(self, optimizer: torch.optim) -> object:
-        """
-        Set optimizer
-        :param optimizer: optimizer object
-        """
-        self.__optimizer = optimizer
-        return self
+            self.__learning_rate = self.LearningRate(config, is_continue=config['start_from'] == 'continue')
 
-    def set_loss(self, loss: torch.nn.Function) -> object:
-        """
-        Set optimizer
-        :param loss: loss object
-        """
-        self.__loss = loss
-        return self
+            self.__optimizer_fnc = getattr(torch.optim, config['optimizer'])
+            if config['optimizer'] == "Adam":
+                self.__optimizer = self.__optimizer_fnc(params=self.__model.model().parameters(), weight_decay=0.0005, eps=10e-10, lr=self.__learning_rate.value())
+            else:
+                self.__optimizer = self.__optimizer_fnc(params=self.__model.model().parameters(), weight_decay=0.0005, lr=self.__learning_rate.value())
 
-    def predict(self, data: torch.Tensor, is_train: bool = False) -> object:
+            self.__epoch_num = 0
+
+        if self.__is_cuda:
+            self.__model.to_cuda()
+
+        if config['start_from'] == 'continue':
+            state_manager = StateManager(file_struct_manager, preffix="best")
+            state_manager.unpack()
+            self.load_weights(state_manager.get_files()['weights_file'])
+            if for_train:
+                self.load_state(state_manager.get_files()['state_file'])
+            state_manager.clear_files()
+
+    def model(self):
+        """
+        Get model
+        """
+        return self.__model.model()
+
+    def learning_rate(self) -> LearningRate:
+        return self.__learning_rate
+
+    def predict(self, data, is_train=False, prev_pose: np.array = None) -> np.array:
         """
         Make predict by data
-        :param data: input for model
+        :param prev_pose: previous position
+        :param data: data in dict
+        :param is_train: is data processor need train on data or just predict
         :return: processed output
         """
-        if is_train:
-            with torch.no_grad():
-                return self.__model(data)
-        return self.__model(data)
 
-    def process_batch(self, batch: {}) -> None:
+        def make_predict(prev_pose=None):
+            if prev_pose is None:
+                prev_pose = torch.cat([data['target']['prev']['pose'], data['target']['prev']['orientation']], dim=1)
+            else:
+                prev_pose = torch.from_numpy(prev_pose)
+
+            if self.__is_cuda:
+                dict_recursive_bypass(data['data'], lambda v: v.to('cuda:0'))
+                prev_pose = prev_pose.to('cuda:0')
+            return self.__model({'prev_img': data['data']['prev_img'], 'cur_img': data['data']['cur_img'], 'prev_pos': prev_pose})
+
+        if is_train:
+            self.__model.model().train()
+            output = make_predict(prev_pose)
+        else:
+            with torch.no_grad():
+                output = make_predict(prev_pose)
+            self.__model.model().eval()
+
+        return output
+
+    def process_batch(self, batch: {}, is_train) -> None:
         """
-        Process one batch odf data
-        :param batch: dta for process
+        Process one batch of data
+        :param batch: dict, contains data and target keys
+        :param is_train: is data processor need to train on data or validate
         """
+        if self.__is_cuda:
+            dict_recursive_bypass(batch['target'], lambda v: v.to('cuda:0'))
+
+        if is_train:
+            self.__optimizer.zero_grad()
+
         self.__optimizer.zero_grad()
 
-        output = self.predict(batch['data'])
+        res = self.predict(batch, is_train)
+        self.metrics_processor.calc_metrics(res, batch['target'], is_train)
 
-        loss = self.__loss(output, batch['target'])
-        loss.backward()
-        self.__optimizer.step()
+        loss = self.__criterion(res, batch['target'])
+        if is_train:
+            loss.backward()
+            self.__optimizer.step()
 
-        self.__train_pipeline.metrics_processor().calculate_metrics(output, batch['target'])
+        self.metrics_processor.set_loss(loss.data[0], is_train)
 
-    def train_epoch(self, train_dataloader, validation_dataloader) -> None:
+    def train_epoch(self, train_dataloader, validation_dataloader, epoch_idx: int, msg: str = None) -> {}:
         """
         Train one epoch
         :param train_dataloader: dataloader with train dataset
         :param validation_dataloader: dataloader with validation dataset
+        :param epoch_idx: index of epoch
+        :return: dict of events
         """
-        for batch in tqdm(train_dataloader, desc="train", leave=False):
-            self.process_batch(batch)
-        for batch in tqdm(validation_dataloader, desc="validation", leave=False):
-            self.process_batch(batch)
+        for batch in tqdm(train_dataloader, desc="train" if msg is None else "train_" + msg, leave=False):
+            self.process_batch(batch, is_train=True)
+        for batch in tqdm(validation_dataloader, desc="validation" if msg is None else "validation_" + msg, leave=False):
+            self.process_batch(batch, is_train=False)
 
-    def __update_lr(self, lr) -> None:
+        self.__epoch_num = epoch_idx
+
+    def update_lr(self, lr: float) -> None:
         """
         Provide learning rate decay for optimizer
         :param lr: target learning rate
@@ -140,18 +210,44 @@ class DataProcessor:
         """
         Get model and optimizer state dicts
         """
-        pass
+        return {'weights': self.__model.model().state_dict(), 'optimizer': self.__optimizer.state_dict()}
 
-    def load_state(self, optimizer_state_path: str) -> None:
+    def get_last_epoch_idx(self):
+        return self.__epoch_num
+
+    def load_state(self, optimizer_state_path: str, with_cur_lr: bool=False) -> None:
         """
         Load state of darta processor. Model state load separately
+        :param with_cur_lr: is need to load learning rate
         :param optimizer_state_path: path to optimizer state path
         """
-        pass
+        print("Data processor inited by file: ", optimizer_state_path, end='; ')
+        state = torch.load(optimizer_state_path)
+        print('state dict len before:', len(state), end='; ')
+        state = {k: v for k, v in state.items() if k in self.__optimizer.state_dict()}
+        print('state dict len after:', len(state), end='; ')
+        self.__optimizer.load_state_dict(state)
+        print('done')
+
+        with open(self.__file_struct_manager.data_processor_state_file(), 'r') as in_file:
+            dp_state = json.load(in_file)
+            self.__epoch_num = dp_state['last_epoch_idx']
+            if not with_cur_lr:
+                self.update_lr(dp_state['lr'])
+                self.__learning_rate.set_value(dp_state['lr'])
+
+    def load_weights(self, path):
+        self.__model.load_weights(path)
+
+    def save_weights(self):
+        self.__model.save_weights()
 
     def save_state(self):
         """
         Save state of optimizer and perform epochs number
         :return:
         """
-        pass
+        torch.save(self.__optimizer.state_dict(), self.__file_struct_manager.optimizer_state_file())
+
+        with open(self.__file_struct_manager.data_processor_state_file(), 'w') as out:
+            json.dump({"last_epoch_idx": self.__epoch_num, 'lr': self.__learning_rate.value()}, out)
--- neural_pipeline/data_processor/monitoring.py
+++ neural_pipeline/data_processor/monitoring.py
@@ -2,7 +2,9 @@ import os
 
 from tensorboardX import SummaryWriter
 import numpy as np
-from neural_pipeline.tonet import FileStructManager
+
+from neural_pipeline.data_processor.model import Model
+from neural_pipeline.utils.file_structure_manager import FileStructManager
 
 
 class Monitor:
@@ -11,22 +13,21 @@ class Monitor:
     metric is np.array - it will be shown as histogram and scalars (scalar plots contains mean valuse from array).
     """
 
-    def __init__(self, config: {}, file_struct_manager: FileStructManager, is_continue: bool, network_name: str = None):
+    def __init__(self, file_struct_manager: FileStructManager, is_continue: bool, start_epoch_idx: int = 0, network_name: str = None):
         """
-        :param config: config
         :param file_struct_manager: file structure manager
         :param is_continue: is data processor continue training
         :param network_name: network name
         """
         self.__writer = None
+        self.__txt_log_file = None
+        self.__epoch_idx = start_epoch_idx
+
         dir = file_struct_manager.logdir_path()
         if dir is None:
             return
 
-        if network_name is None:
-            dir = os.path.join(dir, "{}_{}".format(config['architecture'], config['optimizer']))
-        else:
-            dir = os.path.join(dir, network_name)
+        dir = os.path.join(dir, network_name)
 
         if not is_continue and os.path.exists(dir) and os.path.isdir(dir):
             idx = 0
@@ -38,6 +39,7 @@ class Monitor:
 
         os.makedirs(dir, exist_ok=True)
         self.__writer = SummaryWriter(dir)
+        self.__txt_log_file = open(os.path.join(dir, "log.txt"), 'a' if is_continue else 'w')
 
     def update(self, epoch_idx: int, metrics: {}) -> None:
         """
@@ -45,6 +47,7 @@ class Monitor:
         :param epoch_idx: current epoch index
         :param metrics: metrics
         """
+        self.__epoch_idx = epoch_idx
         self.__update_tensorboard(epoch_idx, metrics)
         self.__update_console(epoch_idx, metrics)
 
@@ -54,20 +57,22 @@ class Monitor:
         :param epoch_idx: index of current epoch
         :param metrics: metrics
         """
-        string = "Epoch: {}".format(epoch_idx + 1)
+        string = "Epoch: [{}]:\n".format(epoch_idx + 1)
 
         for section in ['train', 'validation']:
+            string += section + ':\n'
             for k, v in metrics[section].items():
                 if type(v) == dict:
                     for gk, gv in v.items():
                         if type(gv) == np.ndarray and gv.size > 0:
                             string += ("; {}: [{:4f}, {:4f}, {:4f}]").format("{}_{}".format(k, gk), np.min(gv), np.mean(gv), np.max(gv))
                         else:
-                            string += ("; {}: {:5f}" if type(gv) in [np.float64, float] else "; {}: {}").format("{}_{}".format(k, gk), gv)
+                            string += ("; {}: {:5f}" if type(gv) in [np.float64, float, np.float32] else "; {}: {}").format("{}_{}".format(k, gk), gv)
                 elif type(v) == np.ndarray and v.size > 0:
                     string += ("; {}: [{:4f}, {:4f}, {:4f}]").format(k, np.min(v), np.mean(v), np.max(v))
                 else:
-                    string += ("; {}: {:5f}" if type(v) in [np.float64, float] else "; {}: {}").format(k, v)
+                    string += ("; {}: {:5f}" if type(v) in [np.float64, float, np.float32] else "; {}: {}").format(k, v)
+            string += '\n'
         print(string)
 
     def __update_tensorboard(self, epoch_idx: int, metrics: {}) -> None:
@@ -77,26 +82,50 @@ class Monitor:
         :param metrics: metrics
         """
         def process_value(val):
-            if type(val) == np.ndarray:
-                return np.mean(val)
+            if isinstance(val, np.ndarray) or isinstance(val, list):
+                return float(np.mean(val))
             else:
-                return val
+                return float(val)
 
-        if self.__writer is not None:
-            for section, values in metrics.items():
-                for k, v in values.items():
-                    if type(v) == dict:
-                        for gk, gv in v.items():
-                            if type(gv) == np.ndarray and gv.size > 0:
-                                self.__writer.add_histogram('hist_{}/{}'.format(section, "{}_{}".format(k, gk)), gv, global_step=epoch_idx + 1, bins=np.linspace(0, 1, num=10))
+        if self.__writer is None:
+            return
+
+        for section, values in metrics.items():
+            for k, v in values.items():
+                if type(v) == dict:
+                    for gk, gv in v.items():
+                        if type(gv) == np.ndarray:
+                            if gv.size > 0:
+                                self.__writer.add_histogram('hist_{}/{}'.format(section, "{}_{}".format(k, gk)), np.clip(gv, 0, 1).astype(np.float32), global_step=epoch_idx + 1,
+                                                            bins=np.linspace(-0.2, 1.1, num=14).astype(np.float32))
                                 self.__writer.add_scalars("plt_{}/{}".format(section, k), {gk: process_value(gv) for gk, gv in v.items()}, global_step=epoch_idx + 1)
-                            else:
-                                self.__writer.add_scalars('plt_{}/{}'.format(section, k), {gk: process_value(gv) for gk, gv in v.items()}, global_step=epoch_idx + 1)
-                    elif type(v) == np.ndarray and v.size > 0:
-                        self.__writer.add_histogram('hist_{}/{}'.format(section, k), v, global_step=epoch_idx + 1, bins=np.linspace(0, 1, num=10))
+                        else:
+                            self.__writer.add_scalars('plt_{}/{}'.format(section, k), {gk: process_value(gv) for gk, gv in v.items()}, global_step=epoch_idx + 1)
+                elif type(v) == np.ndarray:
+                    if v.size > 0:
+                        self.__writer.add_histogram('hist_{}/{}'.format(section, k), np.clip(v, 0, 1).astype(np.float32), global_step=epoch_idx + 1,
+                                                    bins=np.linspace(-0.2, 1.1, num=14).astype(np.float32))
                         self.__writer.add_scalar('plt_{}/{}'.format(section, k), process_value(v), global_step=epoch_idx + 1)
-                    else:
-                        self.__writer.add_scalar('plt_{}/{}'.format(section, k), v, global_step=epoch_idx + 1)
+                else:
+                    self.__writer.add_scalar('plt_{}/{}'.format(section, k), float(v), global_step=epoch_idx + 1)
+
+    def write_to_txt_log(self, line: str, tag:str=None):
+        self.__writer.add_text("log" if tag is None else tag, line, self.__epoch_idx)
+        line = "Epoch [{}]".format(self.__epoch_idx) + ": " + line
+        self.__txt_log_file.write(line + '\n')
+        self.__txt_log_file.flush()
+
+    def visualize_model(self, model: Model, tensor) -> None:
+        self.__writer.add_graph(model, tensor)
 
     def close(self):
-        self.__writer.close()
+        if self.__txt_log_file is not None:
+            self.__txt_log_file.close()
+        if self.__writer is not None:
+            self.__writer.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
