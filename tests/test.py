89ec2be822d0d9abdacb31a44b9a225dea5fba0d
toodef <fedotov.anton.af@gmail.com>
2018-12-15 01:14:03 +0300

    fix tests import

--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,7 @@ import unittest
 
 from .data_poducer_test import DataProducerTest
 from .neural_pipeline_test import *
-from ..utils_test import *
+from .utils_test import *
 
 if __name__ == '__main__':
     unittest.main()
