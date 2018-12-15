b40eba799c4d0220071196d5beee1d2a22541240
toodef <fedotov.anton.af@gmail.com>
2018-12-15 18:55:32 +0300

    publication on pypi

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 Neural Pipeline based on PyTorch 0.4.1 and designed to standardize and facilitate the training process.
 
-[![Build Status](https://travis-ci.org/toodef/neural-pipeline.svg?branch=master)](https://travis-ci.org/toodef/neural-pipeline)
+[![Build Status](https://travis-ci.org/toodef/neural-pipeline.svg?branch=master)](https://travis-ci.org/toodef/neural-pipeline) [![PyPI version](https://badge.fury.io/py/neural-pipeline.svg)](https://badge.fury.io/py/neural-pipeline)
 
 It's contains:
 * Checkpoints management and train process resuming
 * Metrics processing and visualisation
+
+# Installation:
+`pip install neural-pipeline`
+
+
+
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="neural_pipeline",
-    version="0.0.1",
-    author="Anton Fedotov: @toodef",
+    name="neural-pipeline",
+    version="0.0.2",
+    author="Anton Fedotov",
     author_email="anton.fedotov.af@gmail.com.com",
     description="Neural Networks train pipeline based on PyTorch",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="none",
+    url="https://github.com/toodef/neural-pipeline",
     packages=setuptools.find_packages(exclude=['tests']),
     install_requires=['numpy', 'tensorboardX', 'tqdm', 'torch==0.4.1'],
     classifiers=[
@@ -20,13 +20,6 @@ setuptools.setup(
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         "Operating System :: OS Independent",
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.0',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
