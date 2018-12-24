5db0b7bfa05be222e05ebadd416c4a3b3ceb7093
toodef <fedotov.anton.af@gmail.com>
2018-12-24 23:14:50 +0300

    small fixes

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,9 @@
 import unittest
 
-from .data_poducer_test import DataProducerTest
+from .data_producer_test import DataProducerTest
 from .train_config_test import *
 from .utils_test import *
+from .data_processor_test import *
 
 if __name__ == '__main__':
     unittest.main()
