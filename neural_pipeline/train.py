2f042b3c6407694387dbc168dbf6241a238199e7
toodef <fedotov.anton.af@gmail.com>
2019-01-21 00:09:00 +0300

    add learning rate decaying

--- neural_pipeline/train.py
+++ neural_pipeline/train.py
@@ -6,6 +6,84 @@ from neural_pipeline.train_config.train_config import TrainConfig
 from neural_pipeline.data_processor.state_manager import StateManager
 from neural_pipeline.monitoring import MonitorHub, ConsoleMonitor
 
+__all__ = ['Trainer', 'LearningRate', 'DecayingLR']
+
+
+class LearningRate:
+    """
+    Basic learning rate class.
+    """
+
+    def __init__(self, value: float):
+        self._value = value
+
+    def value(self) -> float:
+        """
+        Get value of current learning rate
+        """
+        return self._value
+
+    def set_value(self, value) -> None:
+        """
+        Set lr value
+
+        :param value: lr value
+        """
+        self._value = value
+
+
+class DecayingLR(LearningRate):
+    """
+    This class provide lr decaying by defined metric value (by :arg:`target_value_clbk`). If metric value doesn't update minimum throw defined number of steps -
+    lr decay by defined coefficient.
+
+    :param start_value: start value
+    :param decay_coefficient: coefficient of decaying
+    :param patience: steps before decay
+    :param target_value_clbk: callable, that return target value for lr decaying
+    """
+
+    def __init__(self, start_value: float, decay_coefficient: float, patience: int, target_value_clbk: callable):
+        super().__init__(start_value)
+
+        self._decay_coefficient = decay_coefficient
+        self._patience = patience
+        self._cur_step = 1
+        self._target_value_clbk = target_value_clbk
+        self._cur_min_target_val = None
+
+    def value(self) -> float:
+        """
+        Get value of current learning rate
+
+        :return: learning rate value
+        """
+        metric_val = self._target_value_clbk()
+        if metric_val is None:
+            self._cur_step += 1
+            return self._value
+
+        if self._cur_min_target_val is None:
+            self._cur_min_target_val = metric_val
+
+        if metric_val < self._cur_min_target_val:
+            self._cur_step = 1
+            self._cur_min_target_val = metric_val
+
+        if self._cur_step > 0 and (self._cur_step % self._patience) == 0:
+            self._value *= self._decay_coefficient
+            self._cur_min_target_val = None
+            self._cur_step = 1
+            return self._value
+
+        self._cur_step += 1
+        return self._value
+
+    def set_value(self, value):
+        self._value = value
+        self._cur_step = 0
+        self._cur_min_target_val = None
+
 
 class Trainer:
     """
@@ -31,6 +109,10 @@ class Trainer:
 
         self.monitor_hub = MonitorHub()
 
+        self._data_processor = TrainDataProcessor(self.__model, self.__train_config, self.__file_struct_manager,
+                                                  is_cuda=self.__is_cuda)
+        self._lr = LearningRate(self._data_processor.get_lr())
+
     def set_epoch_num(self, epoch_number: int) -> 'Trainer':
         """
         Define number of training epoch
@@ -48,6 +130,10 @@ class Trainer:
         self.__need_resume = True
         return self
 
+    def enable_lr_decaying(self, coeff: float, patience: int, target_val_clbk: callable) -> 'Trainer':
+        self._lr = DecayingLR(self._data_processor.get_lr(), coeff, patience, target_val_clbk)
+        return self
+
     def train(self) -> None:
         """
         Train model
@@ -55,34 +141,42 @@ class Trainer:
         if len(self.__train_config.stages()) < 1:
             raise self.TrainerException("There's no sages for training")
 
-        data_processor = TrainDataProcessor(self.__model, self.__train_config, self.__file_struct_manager, is_cuda=self.__is_cuda)
         state_manager = StateManager(self.__file_struct_manager)
 
         if self.__need_resume:
             state_manager.unpack()
-            data_processor.load()
+            self._data_processor.load()
             state_manager.pack()
 
-        start_epoch_idx = data_processor.get_last_epoch_idx() + 1 if data_processor.get_last_epoch_idx() > 0 else 0
+        start_epoch_idx = self._data_processor.get_last_epoch_idx() + 1 if self._data_processor.get_last_epoch_idx() > 0 else 0
 
         self.monitor_hub.add_monitor(ConsoleMonitor())
 
-        losses = {}
         with self.monitor_hub:
             for epoch_idx in range(start_epoch_idx, self.__epoch_num + start_epoch_idx):
+                losses = {}
                 for stage in self.__train_config.stages():
-                    stage.run(data_processor)
+                    stage.run(self._data_processor)
 
-                    losses[stage.name()] = data_processor.get_losses()
+                    losses[stage.name()] = self._data_processor.get_losses()
                     if stage.metrics_processor() is not None:
                         self.monitor_hub.update_metrics(epoch_idx, stage.metrics_processor().get_metrics())
-                    self._reset_metrics(data_processor)
+                    self._reset_metrics(self._data_processor)
 
-                data_processor.save_state()
+                self._data_processor.save_state()
                 state_manager.pack()
 
                 self.monitor_hub.update_losses(epoch_idx, losses)
-                losses = {}
+
+                self._data_processor.update_lr(self._lr.value())
+
+    def data_processor(self) -> TrainDataProcessor:
+        """
+        Get data processor object
+
+        :return: data processor
+        """
+        return self._data_processor
 
     def _reset_metrics(self, data_processor: TrainDataProcessor) -> None:
         """
--- tests/train_test.py
+++ tests/train_test.py
@@ -6,6 +6,7 @@ import torch
 
 from neural_pipeline import Trainer
 from neural_pipeline.data_processor import Model
+from neural_pipeline.train import DecayingLR
 from neural_pipeline.train_config import TrainConfig, TrainStage, MetricsProcessor
 from neural_pipeline.train_config.train_config import ValidationStage
 from neural_pipeline.utils.file_structure_manager import FileStructManager
@@ -42,8 +43,63 @@ class TrainTest(unittest.TestCase):
                                                 for _ in list(range(20))]]), metrics_processor),
                   ValidationStage(TestDataProducer([[{'data': torch.rand(1, 3), 'target': torch.rand(1)}
                                                      for _ in list(range(20))]]), metrics_processor)]
-        Trainer(model, TrainConfig(stages, SimpleLoss(), torch.optim.SGD(model.parameters(), lr=1), 'exp'), fsm, is_cuda=False)\
+        Trainer(model, TrainConfig(stages, SimpleLoss(), torch.optim.SGD(model.parameters(), lr=1), 'exp'), fsm, is_cuda=False) \
             .set_epoch_num(1).train()
+
+    def test_decaynig_lr(self):
+        step_num = 0
+
+        def target_value_clbk() -> float:
+            return 1 / step_num
+
+        lr = DecayingLR(0.1, 0.5, 3, target_value_clbk)
+        old_val = None
+        for i in range(1, 30):
+            step_num = i
+            value = lr.value()
+            if old_val is None:
+                old_val = value
+                continue
+
+            self.assertAlmostEqual(value, old_val, delta=1e-6)
+            old_val = value
+
+        step_num = 0
+
+        def target_value_clbk() -> float:
+            return 1
+
+        lr = DecayingLR(0.1, 0.5, 3, target_value_clbk)
+        old_val = None
+        for i in range(1, 30):
+            step_num = i
+            value = lr.value()
+            if old_val is None:
+                old_val = value
+                continue
+
+            if i % 3 == 0:
+                self.assertAlmostEqual(value, old_val * 0.5, delta=1e-6)
+            old_val = value
+
+    def test_lr_decaying(self):
+        fsm = FileStructManager(checkpoint_dir_path=self.checkpoints_dir, logdir_path=self.logdir, prefix=None)
+        model = SimpleModel()
+        metrics_processor = MetricsProcessor()
+        stages = [TrainStage(TestDataProducer([[{'data': torch.rand(1, 3), 'target': torch.rand(1)}
+                                                for _ in list(range(20))]]), metrics_processor),
+                  ValidationStage(TestDataProducer([[{'data': torch.rand(1, 3), 'target': torch.rand(1)}
+                                                     for _ in list(range(20))]]), metrics_processor)]
+        trainer = Trainer(model, TrainConfig(stages, SimpleLoss(), torch.optim.SGD(model.parameters(), lr=0.1), 'exp'),
+                          fsm, is_cuda=False).set_epoch_num(10)
+
+        def target_value_clbk() -> float:
+            return 1
+
+        trainer.enable_lr_decaying(0.5, 3, target_value_clbk)
+        trainer.train()
+
+        self.assertAlmostEqual(trainer.data_processor().get_lr(), 0.1 * (0.5 ** 3), delta=1e-6)
 
     def tearDown(self):
         if os.path.exists(self.logdir):
