c8f9f4ded15c23a731934fe3e92b6588ef1a3512
toodef <fedotov.anton.af@gmail.com>
2019-01-20 19:11:46 +0300

    exclude builtin module from global init for exclude from testing

--- neural_pipeline/__init__.py
+++ neural_pipeline/__init__.py
@@ -4,7 +4,6 @@ from . import data_producer
 from . import data_processor
 from . import train_config
 from . import utils
-from .builtin import *
 from .monitoring import MonitorHub, AbstractMonitor, ConsoleMonitor
 from .train import Trainer
 from .predict import Predictor
